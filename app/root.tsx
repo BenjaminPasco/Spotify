f529d4b806af608a295e8baed8a9450cc0180f1d
bpasco <benjamin.pasco@hotmail.fr>
2024-09-20 23:50:39 +0200

    feat: add upload and audio reader

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,50 @@
-import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import {
+	type ActionFunctionArgs,
+	type LoaderFunctionArgs,
+	json,
+} from "@remix-run/node";
+import {
+	Form,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	useLoaderData,
+} from "@remix-run/react";
+import { ensureBucketExists, minioClient } from "utils/minio";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const formData = await request.formData();
+	const file = formData.get("file");
+	if (file && file instanceof File) {
+		const buffer = await file.arrayBuffer();
+
+		await ensureBucketExists("music");
+		await minioClient.putObject(
+			"music",
+			file.name,
+			Buffer.from(buffer),
+			file.size,
+			{ "Content-Type": file.type },
+		);
+
+		return json({ succes: true, message: "File upload success" });
+	}
+	return json({ succes: false, message: "File upload failed" });
+};
+
+export const loader = async (args: LoaderFunctionArgs) => {
+	const statObject = await minioClient.statObject("music", "music.mp3");
+	const fileUrl = await minioClient.presignedGetObject(
+		"music",
+		"music.mp3",
+		60 * 60,
+	);
+	return json({ success: true, fileUrl });
+};
 
 export default function App() {
+	const data = useLoaderData<typeof loader>();
 	return (
 		<html lang="en">
 			<head>
@@ -9,7 +53,16 @@ export default function App() {
 				<Links />
 			</head>
 			<body>
-				<h1>Test if new image is picked up</h1>
+				<h1>FileUpload</h1>
+				<Form method="post" encType="multipart/form-data">
+					<input type="file" name="file" />
+					<button type="submit">Upload</button>
+				</Form>
+				<audio controls src={data.fileUrl}>
+					<source src={data.fileUrl} type="audio/mp3" />
+					Your browser does not support the audio element.
+					<track kind="captions" src="" srcLang="en" label="English Captions" />
+				</audio>
 				<Outlet />
 				<Scripts />
 			</body>
