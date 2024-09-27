b5f416fa25f0552766a56da82c94def5b82160de
bpasco <benjamin.pasco@hotmail.fr>
2024-09-28 00:06:16 +0200

    fix: strenghen minio client initializion and lib functions

--- app/.server/minio.ts
+++ app/.server/minio.ts
@@ -1,18 +1,28 @@
 import { Client } from "minio";
-export const minioClient = new Client({
-	endPoint: process.env.MINIO_HOST || "localhost",
-	port: Number(process.env.MINIO_PORT) || 9000,
-	useSSL: false,
-	accessKey: process.env.MINIO_ACCESS_KEY || "minioadmin",
-	secretKey: process.env.MINIO_SECRET_KEY || "minioadmin",
-	pathStyle: true,
-});
-console.log("Minio client initialized", !!minioClient);
+import invariant from "tiny-invariant";
+
+let minioClient: Client | null = null;
+
+try {
+	minioClient = new Client({
+		endPoint: process.env.MINIO_HOST || "localhost",
+		port: Number(process.env.MINIO_PORT) || 9000,
+		useSSL: false,
+		accessKey: process.env.MINIO_ACCESS_KEY || "minioadmin",
+		secretKey: process.env.MINIO_SECRET_KEY || "minioadmin",
+		pathStyle: true,
+	});
+	console.log("Minio client initialized", !!minioClient);
+} catch (error) {
+	console.error("Minio client failed to initialize");
+}
+export { minioClient };
 
 export async function ensureBucketExists(
 	bucketName: string,
 ): Promise<{ error?: string[] }> {
 	try {
+		invariant(minioClient !== null, "Minio client not initialized");
 		const exist = await minioClient.bucketExists(bucketName);
 		if (!exist) {
 			await minioClient.makeBucket(bucketName, "eu-west-1");
@@ -43,10 +53,11 @@ export async function pushMusic({
 	size: number;
 	type: string;
 }): Promise<{
-	uploadedObject: null | Awaited<ReturnType<typeof minioClient.putObject>>;
+	uploadedObject: null | Awaited<ReturnType<Client["putObject"]>>;
 	error?: string[];
 }> {
 	try {
+		invariant(minioClient !== null, "Minio client not initialized");
 		const { error } = await ensureBucketExists(bucket);
 		if (error) {
 			return {
@@ -73,17 +84,27 @@ export async function pushMusic({
 export async function getMusicFiles({
 	bucket,
 	objectStorageIds,
-}: { bucket: string; objectStorageIds: string[] }) {
-	return await Promise.all(
-		// objectStorageIds.map((id) => minioClient.statObject(bucket, id)),
-		objectStorageIds.map((id) => minioClient.presignedGetObject(bucket, id)),
-	);
+}: { bucket: string; objectStorageIds: string[]; error?: string[] }) {
+	try {
+		return await Promise.all(
+			objectStorageIds.map((id) => {
+				invariant(minioClient !== null, "Minio client is not initialized");
+				return minioClient.presignedGetObject(bucket, id);
+			}),
+		);
+	} catch (error) {
+		return { error: [error] };
+	}
 }
 
 export async function getMusicLink({
 	bucket,
 	objectStorageId,
-}: { bucket: string; objectStorageId: string }) {
-	// objectStorageIds.map((id) => minioClient.statObject(bucket, id)),
-	return minioClient.presignedGetObject(bucket, objectStorageId);
+}: { bucket: string; objectStorageId: string; error?: string[] }) {
+	try {
+		invariant(minioClient !== null, "Minio client is not initialized");
+		return minioClient.presignedGetObject(bucket, objectStorageId);
+	} catch (error) {
+		return { error: [error] };
+	}
 }
