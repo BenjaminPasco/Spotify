2390be8b834a5a7f3aa7b08a3c340f0a16c916f4
bpasco <benjamin.pasco@hotmail.fr>
2024-10-22 23:41:10 +0200

    feat: add favicon to root

--- app/root.tsx
+++ app/root.tsx
@@ -5,6 +5,25 @@ export function links() {
 	return [
 		{ rel: "stylesheet", href: styles },
 		{ rel: "stylesheet", href: "/fonts/inter.css" },
+		{ rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+		{
+			rel: "apple-touch-icon",
+			sizes: "180x180",
+			href: "/apple-touch-icon.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			href: "/favicon-32x32.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "16x16",
+			href: "/favicon-16x16.png",
+		},
+		{ rel: "manifest", href: "/site.webmanifest" },
 	];
 }
 
